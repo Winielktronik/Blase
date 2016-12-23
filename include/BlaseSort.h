aeb9fc9da9ef52a053fae720c109fe8450b600d4
Hansjörg Winterhalter <Hansjrg Winterhalter>
2016-12-23 19:08:15 +0100

    BlaseSort hochladen

---
+++ include/BlaseSort.h
@@ -0,0 +1,12 @@
+/*****************************************************************************
+ * BlaseSort.h
+ *****************************************************************************
+ * Copyright (C) 2016 All Contributors All Rights Reserved.
+ *****************************************************************************/
+//#ifndef INC_LCDINIT_H
+//#define INC_LCDINIT_H
+
+/**
+ * Registers the lcd128 interface in the given driver instance.
+ */
+int LBA-mode(int);
